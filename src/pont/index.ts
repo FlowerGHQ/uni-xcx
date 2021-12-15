286035040d39c206158226641974fad33f13cbba
金王方 <wangfang.jin@xiaobao100.com>
2021-12-15 10:08:33 +0800

    refactor： 接口增加

--- src/pont/index.ts
+++ src/pont/index.ts
@@ -4,19 +4,11 @@ import {
   defs as partnersSBusinessDefs,
   partnersSBusiness
 } from './partnersSBusiness'
-
-import {
-  defs as partnersSBusinessDefs,
-  partnersSBusiness
-} from './partnersSBusiness'
-
 ;(window as any).defs = {
   oauth: oauthDefs,
-  partnersSBusiness: partnersSBusinessDefs,
   partnersSBusiness: partnersSBusinessDefs
 }
 ;(window as any).API = {
   oauth,
-  partnersSBusiness,
   partnersSBusiness
 }
