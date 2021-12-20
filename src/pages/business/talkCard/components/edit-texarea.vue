78e1ca9b11744765176b6a072c1585ba07eb2c23
rui <1744681662@qq.com>
2021-12-20 09:54:48 +0800

    feat:修改输入框

--- src/pages/business/talkCard/components/edit-texarea.vue
+++ src/pages/business/talkCard/components/edit-texarea.vue
@@ -11,8 +11,28 @@
         />
       </div>
       <!-- <van-divider /> -->
-      <div class="textarea-height">
-        <van-field
+      <div class="textarea-recommend">
+        <textarea
+          @linechange="lineChange"
+          placeholder="请输入新的推荐语"
+          placeholder-style="color:#CCCCCC"
+          maxlength="50"
+          :value="textContent"
+          @input="changeTextArea"
+          :disabled="disabled"
+          style="
+            width: 100%;
+            box-sizing: border-box;
+            max-height: 208rpx;
+            overflow: auto;
+            padding: 32rpx;
+            position: relative;
+          "
+        />
+        <div class="text-count-display f f-end">
+          {{ textContent ? textContent.length : 0 }}/50
+        </div>
+        <!-- <van-field
           custom-style="border-top:1px solid #F8F8F8;border-bottom:1px solid #F8F8F8;overflow:auto;"
           type="textarea"
           maxlength="50"
@@ -24,7 +44,7 @@
           :border="true"
           @change="changeTextArea"
         >
-        </van-field>
+        </van-field> -->
       </div>
       <!-- <van-divider /> -->
       <div class="bottom-btn-wrap">
@@ -47,25 +67,44 @@ export default Vue.extend({
   data() {
     return {
       imageCancel: require('@/assets/images/cancel-x.png'),
-      textContent: null
+      textContent: null,
+      lineCount: 0,
+      disabled: false,
+      textLength: 0
     }
   },
   methods: {
     clickCancel() {
       this.$emit('clickCancel')
+      this.textContent = null
+      this.disabled = false
     },
     clickSave() {
+      console.log(this.textContent, 'this.textContent')
       this.$emit('clickSave', this.textContent)
       this.textContent = null
+      this.disabled = false
     },
     changeTextArea(val) {
-      this.textContent = val.detail.trim()
-      // console.log(val.detail.trim(), '去除前后空格哈哈')
+      this.textContent = val.detail.value.trim()
+      // console.log(val, '去除前后空格哈哈')
+    },
+    lineChange(val) {
+      // console.log(val, '换几行')
+      this.lineCount = val.detail.lineCount
+      if (val.detail.lineCount > 3) {
+        this.disabled = true
+      } else {
+        this.disabled = false
+      }
     }
   }
 })
 </script>
 <style lang="less" scoped>
+.textarea-height {
+  display: flex;
+}
 .content {
   width: 750rpx;
   height: 900rpx;
@@ -106,7 +145,23 @@ export default Vue.extend({
   padding: 40rpx 40rpx 80rpx 40rpx;
   box-shadow: 0px 1rpx 0px 0px #dddddd;
 }
-// .van-cell {
-//   height: 300rpx;
-// }
+.f {
+  display: flex;
+}
+.f-end {
+  justify-content: flex-end;
+}
+
+.text-count-display {
+  padding: 2%;
+  font-family: PingFangSC-Regular;
+  font-weight: Regular;
+  font-size: 24rpx;
+  color: #8a8e92;
+}
+
+.textarea-recommend {
+  border-top: 1px solid #f8f8f8;
+  border-bottom: 1px solid #f8f8f8;
+}
 </style>
